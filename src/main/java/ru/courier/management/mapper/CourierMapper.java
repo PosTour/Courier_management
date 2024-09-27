6d7862a20a8c75104d79790140c6b8798e3d97be
posTour <dimidov496@gmail.com>
2024-09-27 23:29:12 +0300

    fix: убрал лишние импорты

--- src/main/java/ru/courier/management/mapper/CourierMapper.java
+++ src/main/java/ru/courier/management/mapper/CourierMapper.java
@@ -1,9 +1,7 @@
 package ru.courier.management.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.courier.management.domain.Courier;
-import ru.courier.management.dto.CoordinateDto;
 import ru.courier.management.dto.CourierDto;
 
 @Mapper
